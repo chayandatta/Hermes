05709898b05c81c77ed2bf0526d0410bb5c2828e
chayandatta <chayan.dutta1996@gmail.com>
2023-05-30 00:17:51 +0530

    lexer newToken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,9 @@
 package lexer
 
+import (
+	"github.com/chayandatta/Hermes/token"
+)
+
 type Lexer struct {
 	input            string
 	position         int  // current input position
@@ -10,7 +14,7 @@ type Lexer struct {
 func New(input string) *Lexer {
 	lex := &Lexer{input: input} // Lexer object creation
 	lex.readCharacter()
-    return lex
+	return lex
 }
 
 func (lex *Lexer) readCharacter() {
@@ -21,4 +25,37 @@ func (lex *Lexer) readCharacter() {
 	}
 	lex.position = lex.readingPosition
 	lex.readingPosition += 1
+}
+
+func (lex *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch lex.currentCharacter {
+	case '=':
+		tok = newToken(token.ASSIGNMENT, lex.currentCharacter)
+	case ';':
+		tok = newToken(token.SEMICOLON, lex.currentCharacter)
+	case '(':
+		tok = newToken(token.LEFT_PARENTHESIS, lex.currentCharacter)
+	case ')':
+		tok = newToken(token.RIGHT_PARENTHESIS, lex.currentCharacter)
+	case ',':
+		tok = newToken(token.COMMA, lex.currentCharacter)
+	case '+':
+		tok = newToken(token.PLUS, lex.currentCharacter)
+	case '{':
+		tok = newToken(token.LEFT_BRACES, lex.currentCharacter)
+	case '}':
+		tok = newToken(token.RIGHT_BRACES, lex.currentCharacter)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	lex.readCharacter()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, char byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(char)}
 }
