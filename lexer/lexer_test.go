c4a509b41d53919ff9b26d588e433c98514c5b38
chayandatta <chayan.dutta1996@gmail.com>
2023-06-03 18:46:20 +0530

    extend testcases

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -16,6 +16,8 @@ func TestNextToken(t *testing.T) {
         };
 
         let result = add(five, ten);
+        !-/*5;
+        5 < 10 > 5;
         `
 
 	tests := []struct {
@@ -57,6 +59,18 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENTIFIER, "ten"},
 		{token.RIGHT_PARENTHESIS, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LESS_THAN, "<"},
+		{token.INT, "10"},
+		{token.GREATER_THAN, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
