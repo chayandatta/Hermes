ff63fdbdd9acaf6d8adb7e7d0fcffe17d0da0b21
chayandatta <chayan.dutta1996@gmail.com>
2023-06-13 00:52:31 +0530

    1.5 repl in action

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/chayandatta/Hermes/repl"
+)
+
+func main() {
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! welcome to the repl of Hermes\n", user.Username)
+	fmt.Print("Start typing to see the repl in action\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
