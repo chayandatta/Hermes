bb6eb7c7b016f9496d602c4c04587b84bf6d6909
chayandatta <chayan.dutta1996@gmail.com>
2023-09-24 22:07:59 +0530

    ast wip

---
+++ parser/parser.go
@@ -0,0 +1,12 @@
+package parser
+
+/* main strategies: top-down parsing or bottom-up parsing.
+“re- cursive descent parsing”, “Early parsing” or “predictive parsing”
+are all variations of top down parsing.
+
+Here we are writing recursive descent parser
+it’s a “top down operator precedence” parser, sometimes called “Pratt parser”,
+after its inventor Vaughan Pratt.
+*/
+
+
